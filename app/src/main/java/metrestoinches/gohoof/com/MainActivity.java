ac9349ce891abc02aa6e7b54afb01a3909cb936a
Rohit Dina Nath <rohitdinanath@gmail.com>
2020-08-20 17:59:41 +0100

    Initial commit

---
+++ app/src/main/java/metrestoinches/gohoof/com/MainActivity.java
@@ -0,0 +1,46 @@
+package metrestoinches.gohoof.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+        private EditText enterMetres;
+        private Button convertButton;
+        private TextView resultTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        enterMetres = (EditText) findViewById(R.id.meterseditText);
+        resultTextView = (TextView) findViewById(R.id.ResultID);
+        convertButton = (Button) findViewById(R.id.convertButtonID);
+
+        convertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double multiplier = 39.37;
+                double result = 0.0;
+
+                if(enterMetres.getText().toString().equals(""))
+                {
+                    resultTextView.setText(R.string.error_message);
+                    resultTextView.setTextColor(Color.RED);
+                }
+                else {
+                    double meterValue = Double.parseDouble(enterMetres.getText().toString());
+                    result = meterValue * multiplier;
+
+                    resultTextView.setTextColor(Color.GREEN);
+                    resultTextView.setText(String.format("%.2f", result) + " inches");
+                }
+            }
+        });
+    }
+}
